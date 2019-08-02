fc65bb1f5829ba43a9fb9d6a6f6c480e6bb41b4c
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2019-08-02 18:00:26 +0200

    winforms: BitmapFontLabel: applied source code header

--- src/csharp/winforms/BitmapFontLabel/BitmapFontLabel/BitmapFont.cs
+++ src/csharp/winforms/BitmapFontLabel/BitmapFontLabel/BitmapFont.cs
@@ -1,3 +1,15 @@
+// License: MIT
+// see LICENSE file in this folder, if this file is not preset refer to
+// https://github.com/sprintworx-gmbh/examples/blob/master/LICENSE
+// https://opensource.org/licenses/MIT
+// https://mit-license.org/
+//
+// The following copyright applies
+//
+// Copyright © 2019 SprintWORX GmbH, authors: Matthias Fritsch and Daniel Bişar
+//
+// <>< he is alive
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
